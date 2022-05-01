ba98f411ee2c8d5c9ff95a76f937f13f89ab03c7
chwinds <104704295+chwinds@users.noreply.github.com>
2022-05-01 11:09:29 +0800

    Add files via upload

---
+++ xue4_30/xue4_30/test.c
@@ -0,0 +1,30 @@
+//
+#include <stdio.h>
+//
+//int main()
+//{
+//	printf("HelloWorld" );
+//		return 0;
+//}
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	int sum = 0;
+//	printf("输入要求和的数：>");
+////	scanf_s ("%d %d", &num1 , &num2);
+////	sum = num1 + num2 ;
+////	printf("sum = %d", sum);
+////	return 0;
+////
+////
+//////}
+//int main()
+//{
+//    int a, b, c , x ;
+//    printf("赋值a，b，c\n");
+//    scanf_s("%d%d%d", &a, &b, &c);
+//    x = a + b + c;
+//    printf("a=%d,b=%d,c=%d,x=%d \n", a, b, c,x );
+//    return 0;
+//}
